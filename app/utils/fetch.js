e9a26b66177a643f53bbadeb89da45dfee821abc
phobal <phobal@126.com>
2019-09-29 14:11:36 +0800

    feat: change source

--- app/utils/fetch.js
+++ app/utils/fetch.js
@@ -1,26 +1,24 @@
-
 import axios from 'axios';
 
-const BASEURL = 'https://iodefog.github.io/text/mviplistmm.json';
+const BASEURL =
+  'https://raw.githubusercontent.com/phobal/ivideo/master/resources/viplist.json';
 
 const instance = axios.create({
   baseURL: BASEURL,
-  timeout: 10000,
+  timeout: 10000
 });
 
 const createAPI = (url, method, config) => {
-  config = config || {};  // eslint-disable-line
+  config = config || {} // eslint-disable-line
   return instance({
     url,
     method,
-    ...config,
+    ...config
   });
 };
 
 const source = {
-  getAllVideoSource: config => createAPI('', 'GET', config),
-}
+  getAllVideoSource: (config) => createAPI('', 'GET', config)
+};
 
-export {
-  source,
-}
+export { source };
